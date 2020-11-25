02d74ccc0188f9a0614e6bc0c0f8bcfb8857cac2
skv93-coder <68867418+skv93-coder@users.noreply.github.com>
2020-11-26 04:10:56 +0530

    Changes

--- src/components/App.js
+++ src/components/App.js
@@ -1,27 +1,16 @@
 import React, {Component, useState} from "react";
-import '../styles/App.css';
-import {
-  BrowserRouter as Router,
-  Switch,
-  useLocation
-} from "react-router-dom";
-import About from "./about";
-import Home from "./home";
+import '../styles/App.css";
+
 class App extends Component {
     render() {
 
         return(
-            <Router>
+            
             <div id="main">
         
-               {/* Do not remove the main div */}
+               
            
-            <Switch>
-            <Router path="/about" Component={About}/>
-            <Router exact path="/" Component={Home}/>
-            </Switch>
             </div>
-            </Router>
         )
     }
 }
--- src/components/home.jsx
+++
@@ -1,5 +0,0 @@
-import React from "react";
-export default function name(params) {
-  return (<><div>You are home.</div></>);
-  
-}
