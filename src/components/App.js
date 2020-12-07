a2241d5306679accf297d041adef33c2481afd9b
skv93-coder <68867418+skv93-coder@users.noreply.github.com>
2020-12-08 00:34:03 +0530

    Change

--- src/components/App.js
+++ src/components/App.js
@@ -1,19 +1,34 @@
 import React, {Component, useState} from "react";
 import '../styles/App.css';
 
+import { BrowserRouter as Router, Link, Route, Switch,useLocation } from "react-router-dom";
+import About from "./About";
+import Home from "./Home";
+import LocationDisplay from "./locationDisplay";
+import NoMatch from "./noMatch";
+
+
+export {LocationDisplay,Home,About,NoMatch};
+
 class App extends Component {
     render() {
 
         return(
-            
             <div id="main">
-        
                
-           
+               <Router>
+                  <Link to="/">Home</Link>
+                  <Link to="/about">About</Link>
+                  <Switch>
+                      <Route path='/' exact component ={Home}/>
+                      <Route path='/about' exact component = {About}/>
+                      <Route component={NoMatch} />
+                  </Switch>
+                  <LocationDisplay/>
+               </Router>
             </div>
         )
     }
 }
-
 
 export default App;
