fbfe241afe01ebfe6a6245b4e892b9b4c8ce8770
skv93-coder <68867418+skv93-coder@users.noreply.github.com>
2020-12-08 00:31:22 +0530

    Create about.js

---
+++ src/components/about.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const About = () => {
+
+      return (
+          <div>
+              You are on the about page
+          </div>
+      )
+}
+
+export default About;
